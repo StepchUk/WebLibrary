93284a28cd4f4be07a9ba516c3f763e9cd3de244
Oleksii_Stepchuk <stepchyk@gmail.com>
2020-10-04 20:01:56 +0300

    Fixed locale lozing

--- src/main/java/loc/stalex/weblibrary/command/CommandLogout.java
+++ src/main/java/loc/stalex/weblibrary/command/CommandLogout.java
@@ -13,11 +13,14 @@ public class CommandLogout implements Command {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String oldUrl = request.getParameter("old-url");
+        String locale = (String) request.getSession().getAttribute("lang");
 
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.invalidate();
         }
+
+        request.getSession(true).setAttribute("lang", locale);
 
         return URLS.REDIRECT.getUrl() + (oldUrl.isEmpty() ? "/" : oldUrl);
     }
